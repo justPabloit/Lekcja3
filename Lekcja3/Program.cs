ab076519b8f6da65166ca6dc1dc3abb1a7457b45
Paweł Piekutowski <futureiscode@gmail.com>
2019-04-18 13:31:23 +0200

    Instrukcje warunkowe IF, ELSE IF

--- Lekcja3/Program.cs
+++ Lekcja3/Program.cs
@@ -10,6 +10,27 @@ namespace Lekcja3
     {
         static void Main(string[] args)
         {
+            // INSTRUKCJE WARUNKOWE
+
+            int a = 4;
+
+            if (a > 0 && a>5)
+            {
+                Console.WriteLine("A jest większe od 0");
+                Console.WriteLine("A jest większe od 5");
+            }
+            else if(a > 0)
+            {
+                Console.WriteLine("A jest większe od 0");
+            }
+            else if(a == 0)
+            {
+                Console.WriteLine("A jest równe 0");
+            }
+            else
+            {
+                Console.WriteLine("A jest mniejsze od 0");
+            }
         }
     }
 }
